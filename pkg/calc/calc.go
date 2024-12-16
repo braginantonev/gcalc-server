cab08dee2d017665ab1947d1a2435884c86e60ff
San4hi <antonsayfermay@gmail.com>
2024-12-16 17:26:24 +0300

    add parseError

--- pkg/calc/calc.go
+++ pkg/calc/calc.go
@@ -25,6 +25,7 @@ var (
 	ExpressionEmpty       error = errors.New("expression empty")
 	OperationWithoutValue error = errors.New("operation dont have a value")
 	BracketsNotFound      error = errors.New("not found opened or closed bracket")
+	ParseError            error = errors.New("parse error")
 )
 
 type Example struct {
@@ -57,7 +58,6 @@ func SolveExample(ex Example) (float64, error) {
 // ! Реализация данной функции довольно таки херовая
 // ! Для оптимизации можно будет попробовать превратить в такую строку где не требуется постоянная замена
 func GetExample(example string) (string, int, Example, error) {
-	var err error
 	var ex Example
 	var local_ex string = example
 
@@ -86,13 +86,17 @@ func GetExample(example string) (string, int, Example, error) {
 	} else if op := "+-"; strings.ContainsAny(local_ex, op) {
 		actionIdx = strings.IndexAny(local_ex, op)
 	} else if strings.ContainsAny(local_ex, "()") {
-		var value float64
-		value, err = strconv.ParseFloat(local_ex[1:len(local_ex)-1], 64)
-		return local_ex[:], strings.IndexRune(example, rune(local_ex[0])), Example{First_value: value, Second_value: 52, Operation: Equals}, err //52 - по рофлу, чтобы при калькулировании не возникала ошибка. Крч костыль
+		value, err := strconv.ParseFloat(local_ex[1:len(local_ex)-1], 64)
+		if err != nil {
+			return "", 0, Example{}, ParseError
+		}
+		return local_ex[:], strings.IndexRune(example, rune(local_ex[0])), Example{First_value: value, Second_value: 52, Operation: Equals}, nil //52 - по рофлу, чтобы при калькулировании не возникала ошибка. Крч костыль
 	} else {
-		var value float64
-		value, err = strconv.ParseFloat(local_ex, 64)
-		return "end", 0, Example{First_value: value, Second_value: 52, Operation: Equals}, err
+		value, err := strconv.ParseFloat(local_ex, 64)
+		if err != nil {
+			return "", 0, Example{}, ParseError
+		}
+		return "end", 0, Example{First_value: value, Second_value: 52, Operation: Equals}, nil
 	}
 
 	if actionIdx == 0 || actionIdx == len(local_ex)-1 {
@@ -110,13 +114,20 @@ func GetExample(example string) (string, int, Example, error) {
 		return "", 0, Example{}, errors.New("action in first or lst place")
 	}
 
+	var err error
 	for i := actionIdx - 1; i >= 0; i-- {
 		if strings.ContainsRune("+-/*()", rune(local_ex[i])) {
 			ex.First_value, err = strconv.ParseFloat(local_ex[i+1:actionIdx], 64)
+			if err != nil {
+				return "", 0, Example{}, ParseError
+			}
 			begin = i + 1
 			break
 		} else if i == 0 {
 			ex.First_value, err = strconv.ParseFloat(local_ex[i:actionIdx], 64)
+			if err != nil {
+				return "", 0, Example{}, ParseError
+			}
 			begin = i
 			break
 		}
@@ -125,17 +136,19 @@ func GetExample(example string) (string, int, Example, error) {
 	for i := actionIdx + 1; i < exampleLen; i++ {
 		if strings.ContainsRune("+-/*()", rune(local_ex[i])) {
 			ex.Second_value, err = strconv.ParseFloat(local_ex[actionIdx+1:i], 64)
+			if err != nil {
+				return "", 0, Example{}, ParseError
+			}
 			end = i
 			break
 		} else if i+1 == exampleLen {
 			ex.Second_value, err = strconv.ParseFloat(local_ex[actionIdx+1:i+1], 64)
+			if err != nil {
+				return "", 0, Example{}, ParseError
+			}
 			end = exampleLen
 			break
 		}
-	}
-
-	if err != nil {
-		return "", 0, Example{}, err
 	}
 
 	return local_ex[begin:end], strings.IndexRune(example, rune(local_ex[0])), ex, nil
