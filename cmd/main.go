f08e7ddf6ec0d92ef5c4ec591c93ef1f6a4d0d73
San4hi <antonsayfermay@gmail.com>
2024-12-20 00:55:56 +0300

    add running application

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/Antibrag/gcalc-server/internal/application"
+)
+
+func main() {
+	app := application.NewApplication()
+	err := app.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
