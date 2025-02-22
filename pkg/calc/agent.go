e66458934436bfd53606b602fdc5d1e6877282ed
San4hi <antonsayfermay@gmail.com>
2025-02-22 13:11:33 +0300

    replace SolveExample() to agent

---
+++ pkg/calc/agent.go
@@ -0,0 +1,21 @@
+package calc
+
+func SolveExample(ex Example) (float64, error) {
+	if ex.Second_value == 0 {
+		return 0, DivideByZero
+	}
+
+	switch ex.Operation {
+	case Plus:
+		return ex.First_value + ex.Second_value, nil
+	case Minus:
+		return ex.First_value - ex.Second_value, nil
+	case Multiply:
+		return ex.First_value * ex.Second_value, nil
+	case Division:
+		return ex.First_value / ex.Second_value, nil
+	case Equals:
+		return ex.First_value, nil
+	}
+	return 0, ExpressionIncorrect
+}
