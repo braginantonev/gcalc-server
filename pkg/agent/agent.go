37109389444a0f82ad903626153c1d37aa28a45a
Bragin Anton <antonsayfermay@gmail.com>
2025-03-03 21:44:57 +0300

    add DHT error to agent

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"slices"
 	"sync"
@@ -14,12 +15,9 @@ import (
 )
 
 const (
-	TIME_ADDITION_MS       = 100
-	TIME_SUBTRACTION_MS    = 150
-	TIME_MULTIPLICATION_MS = 300
-	TIME_DIVISIONS_MS      = 350
-	COMPUTING_POWER        = 5
-	INTERNAL_TASK_URL      = "http://localhost:8080/internal/task"
+	COMPUTING_POWER   = 5
+	TASK_WAIT_TIME_MS = 250
+	INTERNAL_TASK_URL = "http://localhost:8080/internal/task"
 )
 
 type Request struct {
@@ -30,7 +28,7 @@ type Request struct {
 
 var examplesQueue []calc.Example
 
-func EnableAgents(ctx context.Context) {
+func Enable(ctx context.Context) {
 	examplesQueue = make([]calc.Example, 0, 5)
 	mux := sync.Mutex{}
 
@@ -38,34 +36,47 @@ func EnableAgents(ctx context.Context) {
 
 	for range COMPUTING_POWER {
 		go func() {
-			select {
-			case <-ctx.Done():
-				return
+			//Wait enable server
+			<-time.After(1 * time.Second)
 
-			default:
-				ex, err := GetExample()
-				if err != nil {
-					SendRequest(ex, err)
+			for {
+				select {
+				case <-ctx.Done():
+					return
+
+				default:
+					ex, err := GetExample()
+					if err == DHT {
+						<-time.After(TASK_WAIT_TIME_MS * time.Millisecond)
+						continue
+					}
+
+					if err != nil {
+						SendRequest(ex, err)
+					}
+
+					fmt.Println(ex)
+
+					var exampleIdx int
+
+					mux.Lock()
+					if !slices.Contains(examplesQueue, ex) {
+						examplesQueue = append(examplesQueue, ex)
+						exampleIdx = len(examplesQueue) - 1
+					}
+					mux.Unlock()
+
+					if err = Solve(&ex); err != nil {
+						SendRequest(ex, err)
+					}
+
+					mux.Lock()
+					examplesQueue = append(examplesQueue[:exampleIdx], examplesQueue[exampleIdx+1:]...)
+					mux.Unlock()
+
+					fmt.Println(len(examplesQueue))
+					SendRequest(ex, nil)
 				}
-
-				var exampleIdx int
-
-				mux.Lock()
-				if !slices.Contains(examplesQueue, ex) {
-					examplesQueue = append(examplesQueue, ex)
-					exampleIdx = len(examplesQueue) - 1
-				}
-				mux.Unlock()
-
-				if err = Solve(&ex); err != nil {
-					SendRequest(ex, err)
-				}
-
-				mux.Lock()
-				examplesQueue = append(examplesQueue[:exampleIdx], examplesQueue[exampleIdx+1:]...)
-				mux.Unlock()
-
-				SendRequest(ex, nil)
 			}
 		}()
 	}
@@ -74,15 +85,21 @@ func EnableAgents(ctx context.Context) {
 func GetExample() (calc.Example, error) {
 	resp, err := http.Get(INTERNAL_TASK_URL)
 	if err != nil {
+		fmt.Println("get error", err.Error())
 		return calc.Example{}, err
+	}
+
+	if resp.StatusCode == http.StatusNotFound {
+		return calc.Example{}, DHT
 	}
 
 	resp_json := make([]byte, 1024)
 	n, err := resp.Body.Read(resp_json)
-	if err != nil {
+	if err != nil && err != io.EOF {
+		fmt.Println(fmt.Sprint(resp_json))
 		return calc.Example{}, err
 	}
-	resp_json = resp_json[n:]
+	resp_json = resp_json[:n]
 
 	var example calc.Example
 	if err = json.Unmarshal(resp_json, &example); err != nil {
@@ -110,29 +127,24 @@ func Solve(ex *calc.Example) error {
 		return ErrDivideByZero
 	}
 
+	<-time.After(ex.OperationTime * time.Millisecond)
+
 	switch ex.Operation {
 	case calc.Plus:
-		<-time.After(TIME_ADDITION_MS * time.Millisecond)
 		ex.Answer = ex.FirstArgument.Value + ex.SecondArgument.Value
 		return nil
 
 	case calc.Minus:
-		<-time.After(TIME_SUBTRACTION_MS * time.Millisecond)
 		ex.Answer = ex.FirstArgument.Value - ex.SecondArgument.Value
 		return nil
 
 	case calc.Multiply:
-		<-time.After(TIME_MULTIPLICATION_MS * time.Millisecond)
 		ex.Answer = ex.FirstArgument.Value * ex.SecondArgument.Value
 		return nil
 
 	case calc.Division:
-		<-time.After(TIME_DIVISIONS_MS * time.Millisecond)
 		ex.Answer = ex.FirstArgument.Value / ex.SecondArgument.Value
 		return nil
-
-		// case calc.Equals:
-		// 	return ex.FirstArgument.Value, nil
 	}
 	return calc.ErrExpressionIncorrect
 }
--- pkg/agent/errors.go
+++ pkg/agent/errors.go
@@ -3,5 +3,6 @@ package agent
 import "errors"
 
 var (
-	ErrDivideByZero        error = errors.New("divide by zero")
+	ErrDivideByZero error = errors.New("divide by zero")
+	DHT             error = errors.New("don't have task")
 )
