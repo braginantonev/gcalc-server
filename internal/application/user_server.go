3c6f7c8e6ec3190e0e586361dc6eeb1dee7b3426
San4hi <antonsayfermay@gmail.com>
2025-03-05 14:38:38 +0300

    change addExpressionHandler status to created

--- internal/application/user_server.go
+++ internal/application/user_server.go
@@ -51,7 +51,7 @@ func AddExpressionHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusCreated)
 	w.Write(resp_json)
 }
 
