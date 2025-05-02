6453f16ee0ab5c3efa26c0086688ac694766b468
Bragin Anton <antonsayfermay@gmail.com>
2025-05-03 00:29:22 +0300

    add init gRPC server to main

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 
-	"github.com/Antibrag/gcalc-server/internal/application"
+	"github.com/braginantonev/gcalc-server/internal/application"
+	"google.golang.org/grpc"
 )
 
+//Todo: Пофиксить отображение статусов при вызове списка выражений
+//Todo: Заменить все вызовы полей gRPC на гетеры
+//Todo: Все &wrapperspb.StringValue{} заменить на сеттеры - wrapperspb.String()
+//Todo: Почистить код
+
 func main() {
+	grpcServer := grpc.NewServer()
+
 	app := application.NewApplication()
-	err := app.Run()
+	err := app.Run(grpcServer)
 	if err != nil {
 		fmt.Println(err)
 	}
