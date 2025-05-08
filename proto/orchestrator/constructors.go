ba0b4502e8200bbf7d5973c3028d4dea1fe8ee6e
Bragin Anton <antonsayfermay@gmail.com>
2025-05-09 00:38:01 +0300

    add TaskID constructors

--- proto/orchestrator/constructors.go
+++ proto/orchestrator/constructors.go
@@ -2,7 +2,27 @@ package orchestrator
 
 func NewTask() *Task {
 	return &Task{
-		FirstArgument:  &Argument{},
-		SecondArgument: &Argument{},
+		FirstArgument:  &Argument{Expected: -1},
+		SecondArgument: &Argument{Expected: -1},
+	}
+}
+
+func NewTaskResult() *TaskResult {
+	return &TaskResult{
+		TaskID: NewTaskID(),
+	}
+}
+
+func NewTaskID() *TaskID {
+	return &TaskID{
+		Expression: -1,
+		Internal:   -1,
+	}
+}
+
+func NewTaskIDWithValues(expression_id, task_id int32) *TaskID {
+	return &TaskID{
+		Expression: expression_id,
+		Internal:   task_id,
 	}
 }
